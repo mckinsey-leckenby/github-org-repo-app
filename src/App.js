f8371fddf24a98243db9261d3fd922fe4e8b6e5a
mckinseyleckenby <leckenbym11@gmail.com>
2022-04-04 12:34:08 -0500

    updated still not rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react'
+import RepoDetails from './RepoDetails';
+import axios from 'axios'
+import { Card, Button } from 'react-bootstrap'
 
 function App() {
+
+  const [orgName, setOrgName] = useState("")
+  const [loading, setIsLoading] = useState(false)
+  const [repos, setRepos] = useState([])
+  const [details, setDetails] = useState({})
+  const [detailsLoading, setDetailsLoading] = useState(false)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    searchRepos()
+  }
+
+
+  function searchRepos() {
+    setIsLoading(true)
+    axios({
+      method: 'get',
+      url: `https://api.github.com/orgs/${orgName}/repos`,
+    }).then(res => {
+      if (res) {
+        console.log("Worked!!");
+        setIsLoading(false)
+        setRepos(res.data)
+        console.log(res.data)
+      } else {
+        console.log("Did not work!")
+        console.log(res)
+
+      }
+    })
+ }
+
+  function renderRepo(repo) {
+    return (
+      
+      <div className="row" 
+      onClick={() => 
+      console.log("name:", repo.name, 
+      "watchers:", repo.watchers,
+      "forks:", repo.forks,
+      "open issues:", repo.open_issues_count,
+      "license:", repo.license,
+      )} 
+
+    
+      key={repo.id}>
+        <img src={repo.owner.avatar_url} width="40px" />
+        <h2 className='repo-name'>
+          {repo.name.charAt(0).toUpperCase() + repo.name.slice(1)}
+        </h2>
+      </div>
+      
+    )
+  }
+
+  // function getDetails(name) {
+  //   setDetailsLoading(true)
+  //   axios({
+  //     method: 'get',
+  //     url: `https://api.github.com/orgs/${orgName}/${name}`,
+  //   }).then(res => {
+  //     if (res) {
+  //       console.log("Worked!!");
+  //       setDetailsLoading(false)
+  //       setDetails(res.data)
+  //       console.log(res.data)
+  //     } else {
+  //       console.log("Did not work!")
+  //       console.log(res)
+  //     }
+  //   })
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <div className="landing-page-container">
+        <div className="left-side">
+          <form className='form'>
+            <input
+              className='input'
+              value={orgName}
+              placeholder="Search Organizations..."
+              onChange={e => setOrgName(e.target.value)}
+            />
+            <button className="button" onClick={handleSubmit} >{loading ? "Searching..." : "Search"} </button>
+          </form>
+          <div className="results-container">
+            {repos.map(renderRepo)}
+          </div>
+        </div>
+        <RepoDetails details={details} loading={detailsLoading} />
+      </div>
     </div>
   );
 }
---
+++ src/RepoDetails.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function RepoDetails({ details, loading }) {
+    if (loading) {
+        return (
+            <h1 className='loader'>Loading...</h1>
+        )
+    }
+    
+    return (
+        <div className="repo-details-container">
+            <div className='details-row'>
+                <label className="label">Name:</label>
+                <span className='value'>{details.name}</span>
+            </div>
+            <div className='details-row'>
+                <label className="label">Watchers:</label>
+                <span className='value'>{details.watchers}</span>
+            </div>
+            <div className='details-row'>
+                <label className="label">Forks Count:</label>
+                <span className='value'>{details.forks}</span>
+            </div>
+            <div className='details-row'>
+                <label className="label">Open Issues:</label>
+                <span className='value'>{details.open_issues_count}</span>
+            </div>
+            <div className='details-row'>
+                <label className="label">License:</label>
+                <span className='value'>{details.license}</span>
+            </div>
+           
+
+        </div>
+    )
+}
+
+export default RepoDetails
